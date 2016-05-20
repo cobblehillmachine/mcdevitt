3f93178ac5c5276f145f515c1c6fa246bc8e9607
Mary Hipp <maryhipp@Marys-MacBook-Pro.local>
2016-05-20 16:36:29 -0400

    styling single MLS page

--- mls-single.php
+++ mls-single.php
@@ -9,6 +9,7 @@
 
 get_header();the_post(); ?>
 <div class="section border mid-cont mls mls-results">
+	<div class="skinny-cont"><?php dynamic_sidebar('homepage-hero'); ?></div>
 	<div class="gray section">
 		<h2><?php the_title() ?></h2>
 		<?php the_post_thumbnail() ?>
