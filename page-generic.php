82641857dbaf5338329e973361d233406689b106
Mary Hipp <maryhipp@Marys-MacBook-Pro.local>
2016-06-14 08:52:31 -0400

    styling generic SEo page

--- page-generic.php
+++ page-generic.php
@@ -9,6 +9,14 @@
 
 get_header();the_post(); ?>
 <div class="page-generic mid-cont border above-footer">
+	<div class="headlines">
+		<h2><?php the_field('headline') ?></h2>
+		<?php $subheadline = get_field('subheadline') ?>
+		<?php if ($subheadline) { ?>
+			<h3><?php echo $subheadline ?></h3>
+		<?php } ?>
+	</div>
+	
 	<div class="content skinny-cont">
 		<?php the_content(); ?>
 	</div>
