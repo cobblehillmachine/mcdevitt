fa39e1f15547c3cbe9b51e61a0b0143337db95c2
neligajb <boone@cobblehilldigital.com>
2017-06-15 11:44:24 -0400

    open IDX links in new tab

--- page-listings.php
+++ page-listings.php
@@ -7,7 +7,7 @@
 		$name = get_sub_field('name');
 		$idx_number = get_sub_field('idx_link');
 		$image = get_sub_field("background_image") ?>
-			<a style="background-image:url( <?php echo $image ?> )" href="<?php echo $idx_number ?>"><span><?php echo $name ?></span></a>
+			<a target="_blank" style="background-image:url( <?php echo $image ?> )" href="<?php echo $idx_number ?>"><span><?php echo $name ?></span></a>
 		<?php endwhile; ?>
 	</div>
 </div>
